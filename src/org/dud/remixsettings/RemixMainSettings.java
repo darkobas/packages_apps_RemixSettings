d07055ee91edefef5182e70ac0868eb2af9b1e71
darkobas <darkobas@gmail.com>
2015-03-28 22:17:20 +0100

    RemixSettings Init

---
+++ src/org/dud/remixsettings/RemixMainSettings.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2014 The LiquidSmooth Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.dud.remixsettings;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.search.Index;
+import com.android.settings.search.Indexable;
+import android.provider.SearchIndexableResource;
+import java.util.Arrays;
+
+import java.util.List;
+
+public class RemixMainSettings extends SettingsPreferenceFragment implements Indexable {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.remix_main_settings);
+
+	}
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+           Index.getInstance(
+           getActivity().getApplicationContext()).updateFromClassNameResource(
+           RemixMainSettings.class.getName(), true, true);
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+
+    /**
+     * For Search.
+     */
+    public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+        new BaseSearchIndexProvider() {
+
+            @Override
+            public List<SearchIndexableResource> getXmlResourcesToIndex(
+                    Context context, boolean enabled) {
+                final SearchIndexableResource sir = new SearchIndexableResource(context);
+                sir.xmlResId = R.xml.remix_main_settings;
+                return Arrays.asList(sir);
+            }
+	};
+}
